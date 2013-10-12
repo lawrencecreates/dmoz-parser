21456c868a47a2339574b31d139604fe1e2a8c55
James Michael DuPont <jamesmikedupont@gmail.com>
2013-10-12 06:30:10 -0700

    lawrence filter

--- sample.py
+++ sample.py
@@ -3,6 +3,24 @@
 from parser import DmozParser
 from handlers import JSONWriter
 
+class LawrenceFilter:
+  def __init__(self):
+    self._file = open("seeds.txt", 'w')
+
+  def page(self, page, content):
+      if page != None and page != "":
+          topic = content['topic']
+          if topic.find('United_States/Kansas/Localities/L/Lawrence') >  0 :
+              self._file.write(page + "\n")
+              print "found page %s in topic %s" % (page , topic)
+
+  def finish(self):
+    self._file.close()
+
+
 parser = DmozParser()
-parser.add_handler(JSONWriter('output.json'))
+parser.add_handler(
+    LawrenceFilter()
+    #JSONWriter('output.json')
+)
 parser.run()
